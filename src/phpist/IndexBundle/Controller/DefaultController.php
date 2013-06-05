b2cb74cd7b5aa4799ed7990143632464f5028d9a
Osman Yuksel <yuxel@sonsuzdongu.com>
2013-06-05 16:41:57 +0300

    error handler eklendi

--- src/phpist/IndexBundle/Controller/DefaultController.php
+++ src/phpist/IndexBundle/Controller/DefaultController.php
@@ -8,6 +8,12 @@ class DefaultController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('phpistIndexBundle:Default:index.html.twig', array('name' => "osman"));
+        return $this->render('phpistIndexBundle:Default:index.html.twig');
+    }
+ 
+    public function errorAction($request = null,$exception = null)
+    {
+        $code = $exception->getStatusCode();
+        return $this->render('phpistIndexBundle:Default:error.html.twig', array('statusCode' => $code));
     }
 }
