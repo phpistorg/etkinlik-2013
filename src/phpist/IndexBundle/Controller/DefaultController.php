b5f10957dac2378d95286c2466573ad98f5d1f89
Fatih GÜRSOY <fatihgursoy@ymail.com>
2013-07-06 00:15:28 +0300

    Controller Katmanı

--- src/phpist/IndexBundle/Controller/DefaultController.php
+++ src/phpist/IndexBundle/Controller/DefaultController.php
@@ -3,17 +3,37 @@
 namespace phpist\IndexBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+use Acme\StoreBundle\Entity\Speaker;
 
 class DefaultController extends Controller
 {
-    public function indexAction()
-    {
-        return $this->render('phpistIndexBundle:Default:index.html.twig');
-    }
- 
-    public function errorAction($request = null,$exception = null)
-    {
-        $code = $exception->getStatusCode();
-        return $this->render('phpistIndexBundle:Default:error.html.twig', array('statusCode' => $code));
-    }
+	public function indexAction()
+   {
+      $speakers = $this->get('doctrine')
+                  ->getRepository('phpistIndexBundle:Speaker')
+                  ->findAll();
+      
+      return $this->render('phpistIndexBundle:Default:index.html.twig', array('speakers' => $speakers));
+   }
+ 		
+   public function speakerAction($speakerName)
+   {
+      $speaker = $this->get('doctrine')
+               ->getRepository('phpistIndexBundle:Speaker')
+               ->findOneBySlug($speakerName);
+      
+      
+      if(!empty($speaker) && $speaker->getIsActive() == 1) {
+         return $this->render('phpistIndexBundle:Default:speaker.html.twig',array('speaker' => $speaker));
+      }else{
+         throw new \Exception('Something went wrong!');
+      }
+   }
+
+   public function errorAction($request = null,$exception = null)
+   {
+   	$code = $exception->getStatusCode();
+      return $this->render('phpistIndexBundle:Default:error.html.twig', array('statusCode' => $code));
+   }
 }
